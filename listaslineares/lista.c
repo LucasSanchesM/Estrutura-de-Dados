5168bfcc022aeac9d79a34c0762d730e6248c9c4
Lucas Sanches <166553286+LucasSanchesM@users.noreply.github.com>
2026-08-24 21:15:18 -0300

    Add list initialization and element addition functions
    Implement functions to initialize a list and add an element.

---
+++ listaslineares/lista.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+Lista *iniciar(void){
+
+	//Cria um ponteiro do tipo lista e aloca memoria para int
+	Lista *l = malloc(sizeof(Lista));
+	//se ponteiro falhou em alocar memoria, retorna null
+	if(1 == NULL){
+	 	return NULL;
+	}
+
+	//define inicio como null e tamnaho para zero
+	l->inicio = NULL;
+	l->tamanho = 0;
+
+	//retorna o ponteiro para lista
+	return l;
+}
+
+int empty(Lista *l){
+	//retorna true se lista vazia
+	return l -> tamanho == 0;
+}
+
+void add(Lista *l, int numero){
+	if(empty(l)){
+		Elemento *e = malloc(sizeof(Elemento));
+		
+		if(e == NULL){
+			printf("Erro de alocação de memoria");
+			return;
+		}
+		
+		e->valor = numero;
+		e->proximo = NULL;
+		l->inicio = e;
+	}
+}
